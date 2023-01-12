e02cdd6fb8b0e6db0004c624f055d4c082226fef
Shane Spillman <105887707+samsdogjack@users.noreply.github.com>
2023-01-12 00:53:38 -0500

    finished functions_practice

---
+++ functions_practice.py
@@ -0,0 +1,30 @@
+def hello():
+    print("hello")
+
+
+def pack(a, b, c):
+    return [a, b, c]
+    # why does abc work but the numbers 123 don't?
+
+
+def eat_lunch(my_list):
+    if len(my_list) == 0:
+        print("My lunchbox is empty")
+    else:
+        for i in range(len(my_list)):
+         if i == 0:
+            print(f"First I eat {my_list[0]}")
+        else:
+            print(f"Next I eat {my_list[i]}")
+
+hello()
+print(pack("a","b","c"))
+print(pack(1,2,3))
+eat_lunch([])
+eat_lunch(["sandwhich"])
+eat_lunch(["apple","banana","sandwhich","cookie"])
+
+
+
+
+
